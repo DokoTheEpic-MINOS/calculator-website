fda504fd2d300e14cc1f8bf0582dac01b73c2293
DokoTheEpic-MINOS <abadi.attas10@gmail.com>
2025-08-15 12:06:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const display = document.getElementById('display');
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    const lastChar = display.value.slice(-1);
+    if (['+', '-', '*', '/'].includes(lastChar)) {
+        display.value = display.value.slice(0, -1); // Replace last operator
+    }
+    display.value += operator;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = "Error";
+    }
+}
